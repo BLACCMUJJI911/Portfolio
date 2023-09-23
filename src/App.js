0508c16e04ecaf09f5129566f722930b359dccd3
MujtabaBhatti-911 <114852433+BLACCMUJJI911@users.noreply.github.com>
2023-09-23 15:49:12 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Animator, Fade, FadeIn, Move, ScrollContainer, ScrollPage, batch } from "react-scroll-motion";
 import Intro from "./Components/Intro/Intro";
 import Work from "./Components/MyWork/Work";
 
@@ -9,15 +8,9 @@ export default function App(){
 
   return(
     <>
-{/* <ScrollContainer>
-  <ScrollPage page={4}>
-    <Animator animation={batch(Move(1,0))}> */}
+
 
     <Intro/>
-
-    {/* </Animator>
-  </ScrollPage>
-</ScrollContainer> */}
     <Work/>
 
     </>
